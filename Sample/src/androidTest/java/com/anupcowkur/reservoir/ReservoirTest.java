4e1675ea407697b53778dbcd7150ee9a12642139
Anup Cowkur <anupcowkur89@gmail.com>
2015-08-15 15:14:20 +0530

    Adds collection tests and missing rx tests.

--- Sample/src/androidTest/java/com/anupcowkur/reservoir/ReservoirTest.java
+++ Sample/src/androidTest/java/com/anupcowkur/reservoir/ReservoirTest.java
@@ -1,6 +1,6 @@
 package com.anupcowkur.reservoir;
 
-import android.test.suitebuilder.annotation.MediumTest;
+import com.google.gson.reflect.TypeToken;
 
 import com.anupcowkur.reservoirsample.MainActivity;
 import com.anupcowkur.reservoirsample.TestClass;
@@ -10,7 +10,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import android.test.suitebuilder.annotation.MediumTest;
+
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import rx.Observer;
 
@@ -28,6 +34,8 @@ public class ReservoirTest {
 
     private static final String TEST_STRING = "my test string";
     private static final String KEY = "myKey";
+    private static final List<Object> TEST_COLLECTION = new ArrayList<>();
+    private int i;
 
     @Rule
     public final ActivityRule<MainActivity> rule = new ActivityRule<>(MainActivity.class);
@@ -58,6 +66,21 @@ public class ReservoirTest {
 
     @Test
     @MediumTest
+    public void testSyncShouldPutAndGetCollectionObject() throws Exception {
+
+        Reservoir.put(KEY, TEST_COLLECTION);
+
+        Type testResultType = new TypeToken<List<Object>>() {
+        }.getType();
+
+        List<Object> testResultCollection = Reservoir.get(KEY, testResultType);
+
+        assertEquals(TEST_COLLECTION, testResultCollection);
+
+    }
+
+    @Test
+    @MediumTest
     public void testAsyncShouldPutAndGetObject() throws Exception {
 
         TestClass testPutObject = new TestClass();
@@ -71,6 +94,38 @@ public class ReservoirTest {
                     @Override
                     public void onSuccess(TestClass testResultObject) {
                         assertEquals(TEST_STRING, testResultObject.getTestString());
+                    }
+
+                    @Override
+                    public void onFailure(Exception e) {
+                        fail();
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                fail();
+            }
+        });
+
+    }
+
+    @Test
+    @MediumTest
+    public void testAsyncShouldPutAndGetCollectionObject() throws Exception {
+
+        Reservoir.putAsync(KEY, TEST_COLLECTION, new ReservoirPutCallback() {
+            @Override
+            public void onSuccess() {
+
+                final Type testResultType = new TypeToken<List<Object>>() {
+                }.getType();
+
+                Reservoir.getAsync(KEY, testResultType, new ReservoirGetCallback<List<Object>>() {
+                    @Override
+                    public void onSuccess(List<Object> testResultCollection) {
+                        assertEquals(TEST_COLLECTION, testResultCollection);
                     }
 
                     @Override
@@ -129,10 +184,57 @@ public class ReservoirTest {
         });
     }
 
+    @Test
+    @MediumTest
+    public void testAsyncRxShouldPutAndGetCollectionObject() throws Exception {
+
+        final String[] strings = {"one", "two", "three", "four"};
+
+        List<String> testStrings = new ArrayList<>(Arrays.asList(strings));
+
+        i = 0;
+
+        Reservoir.putAsync(KEY, testStrings).subscribe(new Observer<Boolean>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                fail();
+            }
+
+            @Override
+            public void onNext(Boolean success) {
+
+                final Type testResultType = new TypeToken<List<String>>() {
+                }.getType();
+
+                Reservoir.getAsync(KEY, String.class, testResultType).subscribe(new Observer<String>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        fail();
+                    }
+
+                    @Override
+                    public void onNext(String testResultString) {
+                        assertEquals(strings[i++], testResultString);
+                    }
+                });
+            }
+        });
+    }
+
     @Test(expected = NullPointerException.class)
     @MediumTest
     public void testSyncShouldThrowNullPointerExceptionWhenObjectDoesNotExist() throws
-            Exception {
+                                                                                Exception {
 
         Reservoir.get("non_existent_key", TestClass.class);
 
@@ -141,7 +243,7 @@ public class ReservoirTest {
     @Test
     @MediumTest
     public void testAsyncShouldCallOnFailureWhenObjectDoesNotExist() throws
-            Exception {
+                                                                     Exception {
 
         Reservoir.getAsync("non_existent_key", TestClass.class, new ReservoirGetCallback<TestClass>() {
             @Override
@@ -158,8 +260,31 @@ public class ReservoirTest {
 
     @Test
     @MediumTest
+    public void testAsyncRxShouldCallOnFailureWhenObjectDoesNotExist() throws
+                                                                       Exception {
+
+        Reservoir.getAsync("non_existent_key", TestClass.class).subscribe(new Observer<TestClass>() {
+            @Override
+            public void onCompleted() {
+                fail();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                assertThat(e, instanceOf(NullPointerException.class));
+            }
+
+            @Override
+            public void onNext(TestClass testClass) {
+                fail();
+            }
+        });
+    }
+
+    @Test
+    @MediumTest
     public void testSyncShouldThrowIOExceptionWhenObjectSizeGreaterThanCacheSize() throws
-            Exception {
+                                                                                   Exception {
 
         expectedEx.expect(IOException.class);
         expectedEx.expectMessage(SimpleDiskCache.OBJECT_SIZE_GREATER_THAN_CACHE_SIZE_MESSAGE);
@@ -170,7 +295,7 @@ public class ReservoirTest {
     @Test
     @MediumTest
     public void testASyncShouldThrowIOExceptionWhenObjectSizeGreaterThanCacheSize() throws
-            Exception {
+                                                                                    Exception {
 
         Reservoir.putAsync(KEY, TestUtils.getLargeString(), new ReservoirPutCallback() {
             @Override
@@ -182,6 +307,31 @@ public class ReservoirTest {
             public void onFailure(Exception e) {
                 assertThat(e, instanceOf(IOException.class));
                 assertEquals(SimpleDiskCache.OBJECT_SIZE_GREATER_THAN_CACHE_SIZE_MESSAGE, e.getMessage());
+            }
+        });
+
+    }
+
+    @Test
+    @MediumTest
+    public void testASyncRxShouldThrowIOExceptionWhenObjectSizeGreaterThanCacheSize() throws
+                                                                                      Exception {
+
+        Reservoir.putAsync(KEY, TestUtils.getLargeString()).subscribe(new Observer<Boolean>() {
+            @Override
+            public void onCompleted() {
+                fail();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                assertThat(e, instanceOf(IOException.class));
+                assertEquals(SimpleDiskCache.OBJECT_SIZE_GREATER_THAN_CACHE_SIZE_MESSAGE, e.getMessage());
+            }
+
+            @Override
+            public void onNext(Boolean aBoolean) {
+                fail();
             }
         });
 
