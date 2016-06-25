3b6fe9296090f8287a6783125669f1bf921ea4f5
Anup Cowkur <anupcowkur89@gmail.com>
2016-06-25 13:58:20 +0530

    Update UI for sample app

--- Sample/src/main/java/com/anupcowkur/reservoirsample/MainActivity.java
+++ Sample/src/main/java/com/anupcowkur/reservoirsample/MainActivity.java
@@ -2,7 +2,6 @@ package com.anupcowkur.reservoirsample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-
     }
 }
